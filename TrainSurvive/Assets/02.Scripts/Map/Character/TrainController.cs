3d4cb096cb82e9f1d0ee03dcc2ae4a4f17b76495
Strawberryer <646790420@qq.com>
2018-11-13 19:47:47 +0800

    将地图相关的静态变量从TrainController中移出，改善TrainController。
    1、将地图相关的静态变量移到StaticResource中，等待全局变量加入时只需要修改接口即可。
    2、将列车的移动判断的部分函数并入Train中。
    计划：控制器（TrainController）只负责接受指令与发指令，实体（Train）负责具体的行为。
    目前只区分一部分，还没完全区分。

--- TrainSurvive/Assets/02.Scripts/Map/Character/TrainController.cs
+++ TrainSurvive/Assets/02.Scripts/Map/Character/TrainController.cs
@@ -18,39 +18,46 @@ namespace WorldMap
         private const int levelOfTrain = 1;
 
         //私有信息
-        private Vector2Int InitIndex;
-        private Vector2 blockSize;            //readonly
-        private Vector2 mapOrigin;          //readonly
-        private Vector2 mapOriginUnit;    //readonly
-
+        private Vector2 initPosition;
+        private StaticResource staticResource;
         //列车
         private Train train;
         //主摄像机
         private Camera mainCamera;
-        //地图脚本，用于获取地图信息
-        private IMapForTrain iMapForTrain;
-        private IMapForTrainTemp iMapForTrainTemp;
         //主摄像机焦点控制器
         private ICameraFocus cameraFocus;
+        //外部引用
+        private IMapForTrain map;
+        private TeamController teamController;
+        /// <summary>
+        /// 给TraiController设置TeamController
+        /// </summary>
+        /// <param name="tc"></param>
+        public void SetTeamController(TeamController tc)
+        {
+            teamController = tc;
+        }
+        private GameObject trainModeBTs;
         /// <summary>
         /// 一些变量设置
         /// </summary>
         /// <param name="iMapT"></param>
         /// <param name="iMap"></param>
         /// <param name="initPosition">列车初始点</param>
-        public void init(IMapForTrainTemp iMapT, IMapForTrain iMap, Vector2Int initIndex, Train train)
+        public void init(IMapForTrain iMap, Vector2Int initIndex, Train train)
         {
-            iMapForTrainTemp = iMapT;
-            iMapForTrain = iMap;
-            InitIndex = initIndex;
-            blockSize = iMapForTrainTemp.GetBlockSize();
-            Debug.Assert(blockSize.x > 0.1 && blockSize.y > 0.1, "块大小设置的过小");
-            mapOrigin = iMapForTrainTemp.GetMapOrigin();
-            mapOriginUnit = mapOrigin / blockSize + new Vector2(0.5F, 0.5F);
-            train.MinDeltaStep = 0.01F * blockSize.x;
+            staticResource = StaticResource.Instance;
+            map = iMap;
+            initPosition = StaticResource.BlockCenter(initIndex);
+            train.MinDeltaStep = 0.01F * staticResource.BlockSize.x;
             this.train = train;
-            
+            this.train.Init(initPosition);
             ButtonHandler.Instance.AddListeners(this);
+        }
+        void Awake()
+        {
+            Debug.Log("TrainController Awake");
+            trainModeBTs = GameObject.Find("/Canvas").transform.Find("TrainMode").gameObject;
         }
         void Start()
         {
@@ -58,10 +65,9 @@ namespace WorldMap
             mainCamera = Camera.main;
             Debug.Assert(null != mainCamera, "需要将主摄像机的Tag改为MainCamera");
             cameraFocus = mainCamera.GetComponent<ICameraFocus>();
-            Vector2 position = BlockCenter(InitIndex);
-            transform.position = new Vector3(InitIndex.x, levelOfTrain, InitIndex.y);
+            transform.position = StaticResource.MapPosToWorldPos(initPosition, levelOfTrain);
             //驱散迷雾
-            iMapForTrain.MoveToThisSpawn(InitIndex);
+            map.MoveToThisSpawn(StaticResource.BlockIndex(initPosition));
             //焦距自己
             cameraFocus.focusLock(transform);
         }
@@ -78,167 +84,48 @@ namespace WorldMap
                 //因为摄像机的Projection 为 Orthographic，所以Ray的方向都是平行的
                 Debug.Log("origin of ray:" + ray.origin + " dire:" + ray.direction);
                 Debug.Log("mouse position " + Input.mousePosition);
-
-                Vector2 clickedPosition = WorldPosToMapPos(ray.origin);
-                Vector2 currentPosition = WorldPosToMapPos(transform.position);
-                Vector2Int clickIndex = BlockIndex(clickedPosition);
-                Vector2Int currentIndex = BlockIndex(currentPosition);
-                Vector2 startOfRail = new Vector2();
-                Vector2 endOfRail = new Vector2();
-                bool movePositive = true;
-                //判断点击处是否是铁轨
-                if (CanReachableAndSet(ref currentIndex, ref clickIndex, ref startOfRail, ref endOfRail, ref movePositive))
+                if (!train.StartRun(StaticResource.WorldPosToMapPos(ray.origin)))
                 {
-                    train.StartRun(startOfRail, endOfRail, movePositive);
+                    Debug.Log("列车行动失败");
                 }
             }
             //列车允许移动 或者 列车没有停止（列车不一定只有 停止和运行 两个状态） 才能进入
             if (train.IsMovable && !train.IsStoped)
             {
-                Vector2 mapPosition = WorldPosToMapPos(transform.position);
-                Vector2 blockCenter = FormatBlock(mapPosition);
-                if (train.Run(ref mapPosition, blockCenter))
+                Vector2 mapPosition = StaticResource.WorldPosToMapPos(transform.position);
+                if (train.Run(ref mapPosition))
                 {
-                    transform.position = MapPosToWorldPos(mapPosition);
-                    Vector2Int trainMapIndex = BlockIndex(mapPosition);
+                    transform.position = StaticResource.MapPosToWorldPos(mapPosition, levelOfTrain);
+                    Vector2Int trainMapIndex = StaticResource.BlockIndex(mapPosition);
                     //驱散迷雾
-                    iMapForTrain.MoveToThisSpawn(trainMapIndex);
+                    map.MoveToThisSpawn(trainMapIndex);
                 }
             }
         }
-        /// <summary>
-        /// 3D世界坐标转化为地图平面的2D世界坐标
-        /// </summary>
-        /// <param name="worldPosition">3D世界坐标</param>
-        /// <returns></returns>
-        private Vector2 WorldPosToMapPos(Vector3 worldPosition)
+        private bool ActiveTrain(bool active)
         {
-            return Utility.IgnoreY(worldPosition);
-        }
-        /// <summary>
-        /// 地图平面的2D世界坐标转化为3D世界坐标
-        /// </summary>
-        /// <param name="mapPosition">地图平面的2D世界坐标</param>
-        /// <returns></returns>
-        private Vector3 MapPosToWorldPos(Vector2 mapPosition)
-        {
-            return Utility.AcceptY(mapPosition, levelOfTrain);
-        }
-        /// <summary>
-        /// 根据世界坐标计算地图坐标
-        /// </summary>
-        /// <param name="position">世界坐标</param>
-        /// <returns>地图坐标</returns>
-        private Vector2Int BlockIndex(Vector2 position)
-        {
-            //因为块中心位于原点坐标处，所以要减去blockSize/2
-            //(clickedPosition - worldMap.getMapOrigin() - blockSize / 2) / blockSize;
-            //公式优化如下
-            //clicckedPosition/blockSize - ((mapOrigin + blockSize/2)/blockSize)
-            Vector2 index2F = position / blockSize - mapOriginUnit;
-            return new Vector2Int(Mathf.CeilToInt(index2F.x), Mathf.CeilToInt(index2F.y));
-        }
-        /// <summary>
-        /// 计算指定块中心的世界坐标
-        /// </summary>
-        /// <param name="index2d">地图坐标</param>
-        /// <returns>世界坐标</returns>
-        private Vector2 BlockCenter(Vector2Int index2d)
-        {
-            //索引 * 块大小 = 原点到块中心的向量
-            //再加上 原块的中心坐标 就是世界坐标
-            return index2d * blockSize + mapOrigin;
-        }
-        /// <summary>
-        /// 获取当前坐标所在块的中心坐标
-        /// </summary>
-        /// <param name="position">世界坐标</param>
-        /// <returns></returns>
-        private Vector2 FormatBlock(Vector2 position)
-        {
-            return BlockCenter(BlockIndex(position));
-        }
-        /// <summary>
-        /// 判断current和click之间是否连通
-        /// </summary>
-        /// <param name="currentIndex">点1</param>
-        /// <param name="clickIndex">点2</param>
-        /// <param name="startOfRail">铁轨起点</param>
-        /// <param name="endOfRail">铁轨重点</param>
-        /// <param name="movePositive">是否正向移动</param>
-        /// <returns></returns>
-        private bool CanReachableAndSet(ref Vector2Int currentIndex, ref Vector2Int clickIndex, ref Vector2 startOfRail, ref Vector2 endOfRail, ref bool movePositive)
-        {
-            Vector2Int clickedStart, clickedEnd, currentStart, currentEnd;
-            bool trainOnTheTown = iMapForTrain.IfTown(currentIndex);
-            bool clickOnTheTown = iMapForTrain.IfTown(clickIndex);
-            //列车位于城镇上，点击处也是城镇
-            if (trainOnTheTown && clickOnTheTown)
+            if (train.IsMovable == active)
             {
-                //TODO : 最好需要一个接口用来判断两座城市之间有没有铁轨，并且返回起点和终点。
-                Debug.Log("待实现");
+                Debug.Log("重复操作，无效");
                 return false;
             }
-            //列车位于城镇上，点击处不是城镇
-            else if (trainOnTheTown)
+            return train.SetMovable(active);
+        }
+        private bool ActiveBTs(bool active)
+        {
+            if (trainModeBTs.activeSelf == active)
             {
-                if (!iMapForTrain.GetEachEndsOfRail(clickIndex, out clickedStart, out clickedEnd))
-                {
-                    Debug.Log("点击处不是铁轨也不是城镇");
-                    return false;
-                }
-                //点击处铁轨的两端不包括当前列车所在的城市
-                if (currentIndex != clickedStart && currentIndex != clickedEnd)
-                {
-                    Debug.Log("列车所在的城市：" + currentIndex
-                           + " 目标铁轨的城市：" + clickedStart + " => " + clickedEnd + "无法到达");
-                    return false;
-                }
-                currentStart = clickedStart;
-                currentEnd = clickedEnd;
+                Debug.Log("重复操作，无效");
+                return false;
             }
-            //列车不在城镇上，点击处是城镇
-            else if (clickOnTheTown)
-            {
-                if (!iMapForTrain.GetEachEndsOfRail(currentIndex, out currentStart, out currentEnd))
-                {
-                    Debug.Log("列车不在铁轨上也不在城镇上");
-                    return false;
-                }
-                if (clickIndex != currentStart && clickIndex != currentEnd)
-                {
-                    Debug.Log("列车所在的铁轨：" + currentIndex
-                           + " 目标铁轨的城市：" + currentStart + " => " + currentEnd + "无法到达");
-                    return false;
-                }
-                clickedStart = currentStart;
-                clickedEnd = currentEnd;
-            }
-            //列车不在城镇上，点击处也不是城镇
-            else
-            {
-                if (!iMapForTrain.GetEachEndsOfRail(clickIndex, out clickedStart, out clickedEnd))
-                {
-                    Debug.Log("点击处不是铁轨也不是城镇");
-                    return false;
-                }
-                if (!iMapForTrain.GetEachEndsOfRail(currentIndex, out currentStart, out currentEnd))
-                {
-                    Debug.Log("列车不在铁轨上也不在城镇上");
-                    return false;
-                }
-                if (clickedStart != currentStart || clickedEnd != currentEnd)
-                    return false;
-            }
-            Debug.Log("点击处：" + clickIndex + "点击处铁轨的城市：" + clickedStart + "," + clickedEnd);
-            //允许移动
-            startOfRail = BlockCenter(currentStart);
-            endOfRail = BlockCenter(currentEnd);
-            Debug.Log("目前位置：" + currentIndex + "点击处:" + clickIndex);
-            //判断铁轨结尾方向与前往方向是否一致
-            //先判断x轴方向是否一致，再判断z轴方向
-            movePositive = ((currentEnd.x - currentIndex.x) * (clickIndex.x - currentIndex.x) > 0 || (currentEnd.y - currentIndex.y) * (clickIndex.y - currentIndex.y) > 0);
+            trainModeBTs.SetActive(active);
             return true;
+        }
+        public void Active()
+        {
+            ActiveTrain(true);
+            ActiveBTs(true);
+            cameraFocus.focusLock(transform);
         }
         /// <summary>
         /// UI按钮的点击事件，地图的鼠标事件在Update中处理。
@@ -252,11 +139,29 @@ namespace WorldMap
             {
                 case BUTTON_ID.TRAIN_RUN:
                     Debug.Log("开车指令");
-                    train.ContinueRun();
+                    if (!train.ContinueRun())
+                    {
+                        Debug.Log("开车指令失败");
+                        break;
+                    }
                     break;
                 case BUTTON_ID.TRAIN_STOP:
                     Debug.Log("停车指令");
-                    train.StopTemporarily();
+                    if (!train.StopTemporarily())
+                    {
+                        Debug.Log("开车指令失败");
+                        break;
+                    }
+                    break;
+                case BUTTON_ID.TEAM_ACTION:
+                    Debug.Log("探险队行动");
+                    if (!ActiveTrain(false))
+                    {
+                        Debug.Log("探险队行动失败");
+                        break;
+                    }
+                    ActiveBTs(false);
+                    teamController.Active(train.PosTrain);
                     break;
             }
         }
