0b05a877642d8faafeae7d87854b050fde253b9a
Strawberryer <646790420@qq.com>
2018-11-22 23:40:16 +0800

    测试代码： 1、Utility 完成 2、Rail 一半

---
+++ TrainSurvive/Assets/02.Scripts/Editor/Map/Character/TestStaticResource.cs
@@ -0,0 +1,22 @@
+/*
+ * 描述：
+ * 作者：项叶盛
+ * 创建时间：2018/11/19 17:09:40
+ * 版本：v0.1
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStaticResource : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TrainSurvive/Assets/02.Scripts/Editor/Map/TestUtility.cs
@@ -0,0 +1,87 @@
+/*
+ * 描述：
+ * 作者：项叶盛
+ * 创建时间：2018/11/19 17:10:55
+ * 版本：v0.1
+ */
+using NUnit.Framework;
+using UnityEngine;
+
+using WorldMap;
+namespace TestWorldMap
+{
+    [TestFixture]
+    public class TestUtility
+    {
+        [Test]
+        public void TestApproximatelyInView()
+        {
+            //float类型参数
+            float a = 1.0F, b = 1.1F, c = 0.9F;
+            float d = 1.00001F, e = 0.99999F;
+            Assert.IsFalse(Utility.ApproximatelyInView(a, b));
+            Assert.IsFalse(Utility.ApproximatelyInView(a, c));
+            Assert.IsTrue(Utility.ApproximatelyInView(a, d));
+            Assert.IsTrue(Utility.ApproximatelyInView(a, e));
+            //向量类型参数
+            Vector2 va = new Vector2(a, a);
+            Vector2 vaYF = new Vector2(a, b);
+            Vector2 vaYN = new Vector2(a, d);
+            Vector2 vaXF = new Vector2(b, a);
+            Vector2 vaXN = new Vector2(d, a);
+            Assert.IsFalse(Utility.ApproximatelyInView(va, vaYF));
+            Assert.IsTrue(Utility.ApproximatelyInView(va, vaYN));
+            Assert.IsFalse(Utility.ApproximatelyInView(va, vaXF));
+            Assert.IsTrue(Utility.ApproximatelyInView(va, vaXN));
+        }
+        [Test]
+        public void TestIgnoreAndAcceptY()
+        {
+            Vector3 a = new Vector3(1.0F, 1.0F, 1.0F);
+            Vector2 a2 = Utility.IgnoreY(a);
+            Assert.AreEqual(a.x, a2.x);
+            Assert.AreEqual(a.z, a2.y);
+            Assert.AreEqual(a, Utility.AcceptY(a2, a.y));
+        }
+        [Test]
+        public void TestIfBetween()
+        {
+            float edge1 = -1.0F, edge2 = 2.0F;
+            float value1 = edge1, value2 = edge2;
+            float value3 = 1.0F, value4 = 3.0F, value5 = -2.0F;
+            //IfBetweenBoth
+            Assert.IsTrue(Utility.IfBetweenBoth(edge1, edge2, value1));
+            Assert.IsTrue(Utility.IfBetweenBoth(edge1, edge2, value2));
+            Assert.IsTrue(Utility.IfBetweenBoth(edge1, edge2, value3));
+            Assert.IsFalse(Utility.IfBetweenBoth(edge1, edge2, value4));
+            Assert.IsFalse(Utility.IfBetweenBoth(edge1, edge2, value5));
+            //IfBetweenLeft
+            Assert.IsTrue(Utility.IfBetweenLeft(edge1, edge2, value1));
+            Assert.IsFalse(Utility.IfBetweenLeft(edge1, edge2, value2));
+            Assert.IsTrue(Utility.IfBetweenLeft(edge1, edge2, value3));
+            Assert.IsFalse(Utility.IfBetweenLeft(edge1, edge2, value4));
+            Assert.IsFalse(Utility.IfBetweenLeft(edge1, edge2, value5));
+        }
+    }
+    [TestFixture]
+    public class TestMatrix2x2Int
+    {
+        [Test]
+        public void TestRoate()
+        {
+            Vector2Int va = new Vector2Int(2, 3);
+            Assert.AreEqual(Matrix2x2Int.Roate90Clockwise(va), new Vector2Int(3, -2));
+            Assert.AreEqual(Matrix2x2Int.Roate90Anticlockwise(va), new Vector2Int(-3, 2));
+            Assert.AreEqual(Matrix2x2Int.Roate180(va), new Vector2Int(-2, -3));
+            Matrix2x2Int.Roate90Clockwise(ref va);
+            Assert.AreEqual(va, new Vector2Int(3, -2));
+            va = new Vector2Int(2, 3);
+            Matrix2x2Int.Roate90Anticlockwise(ref va);
+            Assert.AreEqual(va, new Vector2Int(-3, 2));
+            va = new Vector2Int(2, 3);
+            Matrix2x2Int.Roate180(ref va);
+            Assert.AreEqual(va, new Vector2Int(-2, -3));
+        }
+    }
+}
+
