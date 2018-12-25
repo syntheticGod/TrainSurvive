596fa1ae1aebe578ceef556965532cb40c43af63
Strawberryer <646790420@qq.com>
2018-12-25 23:18:07 +0800

    1

--- TrainSurvive/Assets/02.Scripts/Map/Model/NPC.cs
+++ TrainSurvive/Assets/02.Scripts/Map/Model/NPC.cs
@@ -17,8 +17,6 @@ namespace WorldMap.Model
     {
         public List<int> taskId_canDo=new List<int>();
         public List<int> taskId_doing= new List<int>();
-        public List<int> taskId_locked = new List<int>();
-        public List<int> taskId_finish = new List<int>();
         public Person PersonInfo { private set; get; }
         public string Name { get { return PersonInfo.name; } }
         public int Strength { get { return PersonInfo.strength; } }
@@ -45,28 +43,12 @@ namespace WorldMap.Model
             }
         }
         /// <summary>
-        /// 给NPC添加能开始的或未解锁的任务
+        /// 给NPC添加能开始的的任务
         /// </summary>
         /// <param name="taskId">任务ID</param>
-        /// <param name="taskCondition">取值范围[LOCKED, CAN_DO]</param>
-        /// <returns>
-        /// TRUE：添加成功
-        /// FALSE：该任务状态的任务不允许被填加
-        /// </returns>
-        public bool AddTask(int taskId, TaskController.TASKCONDITION taskCondition)
+        public void AddCanDoTask(int taskId)
         {
-            switch (taskCondition)
-            {
-                case TaskController.TASKCONDITION.LOCKED:
-                    taskId_locked.Add(taskId);
-                    break;
-                case TaskController.TASKCONDITION.CAN_DO:
-                    taskId_canDo.Add(taskId);
-                    break;
-                default:
-                    return false;
-            }
-            return true;
+            taskId_canDo.Add(taskId);
         }
         //调用taskController的getTask获取任务
 
